fdf2d7c9a192095e5344c09262caf5c12257e110
notaris <80954329+notarisj@users.noreply.github.com>
2023-11-13 00:07:01 +0200

    Include both default and modern class names for the skip button

--- src/content.js
+++ src/content.js
@@ -1,6 +1,10 @@
 setInterval(function() {
-  let skipButton = document.querySelector('.ytp-ad-skip-button-modern');
+  let skipButton = document.querySelector('.ytp-ad-skip-button');
+  let skipButtonModern = document.querySelector('.ytp-ad-skip-button-modern');
+
   if (skipButton) {
     skipButton.click();
+  } else if (skipButtonModern) {
+    skipButtonModern.click();
   }
 }, 1000); // Check every second
