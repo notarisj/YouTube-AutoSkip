efd4667c15b0ae9ecc69aa14eef08988547c77de
notaris <80954329+notarisj@users.noreply.github.com>
2023-11-13 23:58:05 +0200

    Skip unskippable ads by increasing playbackRate. Setting the time manually caused the ad to replay.

--- src/content.js
+++ src/content.js
@@ -1,29 +1,30 @@
+
+// let adOverlay = document.querySelector('.ytp-ad-player-overlay');
+// let adDurationElement = document.querySelector('.ytp-ad-duration-remaining');
+
+// let durationText = adDurationElement.innerText;
+// let durationParts = durationText.split(':').map(part => parseInt(part, 10));
+// let adDurationInSeconds = durationParts.reduce((total, part) => (total * 60) + part, 0);
+
+setInterval(checkAndSkipAds, 1000);
+
 function checkAndSkipAds() {
   chrome.runtime.sendMessage({message: "getState"}, function(response) {
     if (!response.state) {
-      let videoPlayer = document.querySelector('video');
+      let skipButton = document.querySelector('.ytp-ad-skip-button');
+      let skipButtonModern = document.querySelector('.ytp-ad-skip-button-modern');
       let adOverlay = document.querySelector('.ytp-ad-player-overlay');
-      let adDurationElement = document.querySelector('.ytp-ad-duration-remaining');
 
-      if (adOverlay && adDurationElement) {
-        // Extract remaining ad duration
-        let durationText = adDurationElement.innerText;
-        let durationParts = durationText.split(':').map(part => parseInt(part, 10));
-        let adDurationInSeconds = durationParts.reduce((total, part) => (total * 60) + part, 0);
-
-        // Seek to the end of the ad
-        videoPlayer.currentTime += adDurationInSeconds + 1;
-
-        let skipButton = document.querySelector('.ytp-ad-skip-button');
-        let skipButtonModern = document.querySelector('.ytp-ad-skip-button-modern');
-
-        // Click the skip button if shown at the end
-        if (skipButton) {
-          skipButton.click();
-        } else if (skipButtonModern) {
-          skipButtonModern.click();
+      if (skipButton) {
+        skipButton.click();
+      } else if (skipButtonModern) {
+        skipButtonModern.click();
+      } else if (adOverlay) {
+        let videoPlayer = document.querySelector('video');
+        if (videoPlayer) {
+          videoPlayer.muted = true; // Mute the audio
+          videoPlayer.playbackRate = 10; // Speed up playback to quickly pass through unskippable ad
         }
-
       }
     }
   });
